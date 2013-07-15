09ac48644397b17b2d2272aa51ba2adbd3ca6951
phalax4 <jeremylim@sbcglobal.net>
2013-07-15 16:38:34 -0400

    Cleaned up left over CVS

--- src/Access/Reader.java
+++ src/Access/Reader.java
@@ -1,14 +1,14 @@
 package Access;
 
 import java.io.FileWriter;
+
 import java.io.IOException;
-import com.google.gson.stream.JsonWriter;
+
 
 
 public class Reader {
 	
-	 JsonWriter writer;
-	 public void parse(){
+	 	 public void parse(){
 		 
 		 
 	 }
