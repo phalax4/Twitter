919228c3b6e1e5d151dcc24cc6a650ed942c1a92
phalax4 <jeremylim@sbcglobal.net>
2013-07-15 07:22:30 -0400

    Database

---
+++ src/Access/Driver.java
@@ -0,0 +1,36 @@
+package Access;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.Status;
+
+
+public class Driver {
+	
+	
+	public static void main(String args[]) throws Exception {
+		
+		Tweet dave = new Tweet();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Have you already authorized this app? (y/n): ");
+		String dec = sc.nextLine();
+		sc.close();
+		
+		if(dec.equals("n")){
+			dave.start();
+		}else{
+			dave.getCreds();	
+		}
+		dave.homeTimeLn();
+
+	}
+	
+	
+}
